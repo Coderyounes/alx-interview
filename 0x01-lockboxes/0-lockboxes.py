88962480f40776d6f3bea19b010b81abe62f524b
Coderyounes <younesanas2021@gmail.com>
2024-04-18 10:27:22 +0100

    I don't Like it

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,5 +2,26 @@
 
 """ Documentation """
 
-def canUnlockAll():
-    pass
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes in the list can be unlocked.
+
+    Args:
+        boxes (list): A list of lists representing the lockboxes.
+        Each inner list contains the indices of the boxes that can be unlocked
+        with the corresponding key.
+
+    Returns:
+        bool: True if all the boxes can be unlocked, False otherwise.
+    """
+    keys = set([0])
+    visited = set()
+
+    while keys:
+        current_key = keys.pop()
+        visited.add(current_key)
+        keys.update(boxes[current_key])
+        keys -= visited
+
+    return len(visited) == len(boxes)
