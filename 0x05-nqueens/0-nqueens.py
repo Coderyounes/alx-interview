0758694c27c8f76e04f7f5c0f30a0e227f7db420
Coderyounes <younesanas2021@gmail.com>
2024-05-17 21:47:44 +0100

    Prepare the TODO's

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,5 +1,14 @@
 #! /usr/bin/env python3
 
+# TODO: function to Check row, Columns, diagonals if valid
+# TODO: function that initialize the ChessBoard based on N
+# TODO: Usage nqueens N ( take arg CLI)
+# TODO: if nqueens N N1 exit with status 1
+# TODO: Check if N is an int
+# TODO: Check if N is Greater or Equal to 4
+# TODO: The Program should Print every possible solution
+# TODO: We can use 'sys' module
+# TODO: Read About Backtracking
 def main():
     print("Hello Function")
 
