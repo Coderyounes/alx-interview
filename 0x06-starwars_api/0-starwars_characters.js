d30fb9184fe45d4d8723e94913841e7038039ec3
Coderyounes <younesanas2021@gmail.com>
2024-05-23 17:30:54 +0100

    refactor: update Star Wars API URL

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
 
-const API_URL = 'https://swapi.dev/api';
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
-      return;
     }
-
     const charactersURL = JSON.parse(body).characters;
     const charactersName = charactersURL.map(
       (url) => new Promise((resolve, reject) => {
         request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
             reject(promiseErr);
-          } else {
-            resolve(JSON.parse(charactersReqBody).name);
           }
+          resolve(JSON.parse(charactersReqBody).name);
         });
-      })
+      }),
     );
 
     Promise.all(charactersName)
       .then((names) => console.log(names.join('\n')))
       .catch((allErr) => console.log(allErr));
   });
-} else {
-  console.error('Please provide a movie ID');
 }
